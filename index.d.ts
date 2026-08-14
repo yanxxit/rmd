faf6dc5249fc96640177ae026ad0ce0375ce49ba
闫肖肖 <yanxxit@163.com>
2026-08-14 20:27:24 +0800

    chore: 发布 0.1.22 版本

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export function removeSync(targets: string | string[]): void;
-export function removeAsync(targets: string | string[]): Promise<void>;
-export function pathExists(target: string): boolean;
-export const native: unknown;
